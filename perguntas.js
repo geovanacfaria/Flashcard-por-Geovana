a433159451d8670488b2d5e744e450b2009a330d
geovanacfaria <00001129754455sp@al.educacao.sp.gov.br>
2024-11-28 15:18:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Idade Média',
+    'Na Idade Média registrou-se diversos empaces, inclusive na questão sanitária: falta de higiene, de água tratada e de um sistema de esgoto etc, resultando em surtos, que mataram milhares de pessoas. No entanto, houve uma epidemia notória e até hoje é globalmente associada ao período. Qual foi este surto epidêmico?',
+    'A Peste Negra (ou Peste Bubônica).'
+)
+
+criaCartao(
+    'Grandes Impérios',
+    'Durante o Império Romano, no governo de Otávio (27 a.C. a 14 d.C), obras públicas foram erguidas, tropas foram posicionadas em locais estratégicos e a fidelidade das províncias foi garantida. O período caracterizou-se pela relativa concórdia e prosperidade. Como é titulado este governo?',
+    'A Pax Romana. Essa paz foi conquistada por meio de uma série de ações conduzidas pelo império, como a romanização das províncias conquistadas e anexadas etc. Esse período de paz e prosperidade estendeu-se até o final do século II d.C. '
+)
+
+criaCartao(
+    'Brasil Império',
+    'O processo de abolição do trabalho escravo aconteceu de maneira lenta, sendo marcado por uma série de leis que promoveram essa abolição de maneira gradual no país. A primeira lei abolicionista no Brasil concedeu liberdade aos filhos de escravos nascidos a partir de sua promulgação. Que lei é essa? ',
+    'A Lei do Ventre Livre, promulgada em 28 de setembro de 1871, também titulada Lei Rio Branco.'
+)
+
+criaCartao(
+    'República Velha',
+    'República Velha, também conhecida como Primeira República, foi um período da história brasileira que se estendeu de 1889 a 1930 e ficou marcado pela força das oligarquias. Seu fim em 1930, ocorreu por um fato que deu início à Revolta Armada. Qual foi o estopim que desencadeou tal revolta?',
+    'O assassinato de João Pessoa (vice da chapa de Getúlio Vargas, como candidato à presidência) em 1930, foi o estopim da Revolução de 1930, movimento armado que deu fim à Primeira República.'
+)
+
+criaCartao(
+    'Grécia Antiga',
+    'Durante o período da Hélade, também conhecida por Grécia Antiga, sucedeu o conflito militar designado como Guerra do Peloponeso. Foi uma guerra civil que ocorreu entre 431 e 404 a.C. e durou 27 anos. O conflito terminou com a vitória de qual exército?',
+    'Esparta.'
+)
+
+criaCartao(
+    'Revoluções e Revoltas',
+    'A campanha de vacinação conduzida pela Diretoria Geral de Saúde Pública (DGSP) em 1904, buscava erradicar certa doença do Rio de Janeiro. A campanha causou indignação na população e originou o que ficou conhecido por Revolta da Vacina. Esta útima, foi resposta a uma vacinação compulsória para qual doença?',
+    'Varíola.'
+)
+
+criaCartao(
+    'Guerras',
+    'Na primeira metade do século XX, os japoneses realizaram um ataque surpresa contra a base naval norte-americana de Pearl Harbor, objetivando a destruição desta frota naval do Pacífico. No dia seguinte, 8 de dezembro, os EUA declararam guerra ao Japão. Este fato foi decisivo que os EUA entrassem em qual guerra?',
+    'Segunda Guerra Mundia, que durou de 1939 a 1945.'
+)
+
+criaCartao(
+    'Grandes Navegações',
+    'A chegada dos espanhóis à América e as notícias do feito da expedição de Colombo chamaram a atenção de Portugal, país vizinho que, havia décadas, dedicava-se à expedição marítima. Qual foi acordo foi assinado entre o Reino de Castela (Espanha) e o de Portugal que definiu a divisão das novas terras?',
+    'O Tratado de Tordesilhas, realizado no dia 7 de junho de 1494.'
+)
+
+criaCartao(
+    'História da Arte',
+    'A arte romana se desenvolveu na Roma Antiga entre o século 753 a.C. e o séc IV d.C. Sobre ela, é correto pontuar que dois povos foram as principais influências para suas manifestações artísticas. Quem foram estes povos?',
+    'Etruscos e gregos. A maiores influências para os romanos foram a arte grega, com seu senso estético, e os etruscos, com sua objetividade.'
+)
